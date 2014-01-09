c1a8caa8fbc6afe4d807ef1c54e2ab4198c8bd37
Alex Freas <akfreas@gmail.com>
2014-01-09 09:23:47 -0500

    Initial Commit

---
+++ PicWalkTests/PicWalkTests.m
@@ -0,0 +1,34 @@
+//
+//  PicWalkTests.m
+//  PicWalkTests
+//
+//  Created by Alexander Freas on 1/9/14.
+//  Copyright (c) 2014 Sashimiblade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PicWalkTests : XCTestCase
+
+@end
+
+@implementation PicWalkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
